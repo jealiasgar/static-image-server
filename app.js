c3631701c4bf986b3552486be5d6fb0ea96c695d
jealiasgar <pocketwala.ali@gmail.com>
2021-09-02 12:29:32 +0530

    removing unused code

--- app.js
+++
@@ -1,9 +0,0 @@
-let lighter = require("@orderstack/os-lighters")
-
-lighter.glow(async () => {
-	log.d(
-		`Example Lighters Server Running env: ${JSON.stringify({
-			mode: process.argv[2],
-		})}`
-	)
-})
--- config/routes.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-	versions: {
-		default: "v0.1",
-		all: ["v0.1", "v0.2", "v1.0"],
-	},
-}
