2f65b93990e55649fc0ea5f25e10e2085370ed6c
jealiasgar <pocketwala.ali@gmail.com>
2021-09-02 16:49:42 +0530

    feat: GET random image by width and height

--- app.js
+++ app.js
@@ -1,37 +1,49 @@
-var path = require("path")
 var express = require("express")
 var app = express()
 var fs = require("fs")
+var sharp = require("sharp")
 
-var dir = path.join(__dirname, "public")
-
-var mime = {
-	html: "text/html",
-	txt: "text/plain",
-	css: "text/css",
-	gif: "image/gif",
-	jpg: "image/jpeg",
-	png: "image/png",
-	svg: "image/svg+xml",
-	js: "application/javascript",
-}
+var imageData = require("./image-data")
 
 app.get("*", function (req, res) {
-	console.log(req.path)
-	// var file = path.join(dir, req.path.replace(/\/$/, "/index.html"))
-	// if (file.indexOf(dir + path.sep) !== 0) {
-	// 	return res.status(403).end("Forbidden")
-	// }
-	// var type = mime[path.extname(file).slice(1)] || "text/plain"
-	// var s = fs.createReadStream(file)
-	// s.on("open", function () {
-	// 	res.set("Content-Type", type)
-	// 	s.pipe(res)
-	// })
-	// s.on("error", function () {
-	// 	res.set("Content-Type", "text/plain")
-	// 	res.status(404).end("Not found")
-	// })
+	let urlArr = req.path.split("/").filter((e) => e !== "")
+	let random = Math.floor(Math.random() * 6)
+	if (urlArr.length === 0) {
+		var s = fs.createReadStream(imageData[random])
+		s.on("open", function () {
+			res.set("Content-Type", "image/jpeg")
+			s.pipe(res)
+		})
+		s.on("error", function () {
+			res.set("Content-Type", "text/plain")
+			res.status(404).end("Not found")
+		})
+	} else if (urlArr.length === 2 && urlArr[0] !== "id") {
+		let w = Number(urlArr[0])
+		let h = Number(urlArr[1])
+		let inputString = `./public/${random}_${w}_${h}.jpg`
+
+		let inStream = fs.createReadStream(imageData[random])
+		let outStream = fs.createWriteStream(inputString, { flags: "w" })
+		outStream.on("error", function () {
+			console.log("Error")
+		})
+
+		outStream.on("close", function () {
+			console.log("Saved the output file.")
+		})
+
+		let transform = sharp()
+			.resize({ width: w, height: h })
+			.on("info", function () {
+				res.set("Content-Type", "image/jpeg")
+				inStream.pipe(transform).pipe(res)
+			})
+
+		inStream.pipe(transform).pipe(outStream)
+	} else {
+		res.status(404).end("Not Found")
+	}
 })
 
 app.listen(3000, function () {
