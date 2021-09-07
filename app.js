0d0cfa1c6198e5dcb7f3559f706115c442990ac8
jealiasgar <pocketwala.ali@gmail.com>
2021-09-07 12:46:52 +0530

    feat: blur and grayscale functionality

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ var sharp = require("sharp")
 var imageData = require("./image-data")
 
 app.get("*", function (req, res) {
+	if (req.query.blur && Number(req.query.blur) > 20) {
+		res.status(400).end("Blur cannot be more than 20")
+	}
+	let filters = Object.keys(req.query)
 	let urlArr = req.path.split("/").filter((e) => e !== "")
 	let random = Math.floor(Math.random() * 6)
 	if (urlArr.length === 0) {
@@ -19,16 +23,34 @@ app.get("*", function (req, res) {
 			res.status(404).end("Not found")
 		})
 	} else if (urlArr.length === 2 && !isNaN(Number(urlArr[0]))) {
-		createFile(Number(urlArr[0]), Number(urlArr[1]))
+		createFile({
+			w: Number(urlArr[0]),
+			h: Number(urlArr[1]),
+			isGrayscale: filters.includes("grayscale"),
+			// prettier-ignore
+			blur: req.query.blur
+				? Number(req.query.blur)
+				: filters.includes("blur")
+					? 5
+					: 0.3,
+		})
 	} else if (urlArr.length === 1 && !isNaN(Number(urlArr[0]))) {
-		createFile(Number(urlArr[0]), Number(urlArr[0]))
+		createFile({
+			w: Number(urlArr[0]),
+			h: Number(urlArr[0]),
+			isGrayscale: filters.includes("grayscale"),
+			// prettier-ignore
+			blur: req.query.blur
+				? Number(req.query.blur)
+				: filters.includes("blur")
+					? 5
+					: 0.3,
+		})
 	} else {
 		res.status(404).end("Not Found")
 	}
 
-	function createFile(w, h) {
-		// let w = Number(urlArr[0])
-		// let h = Number(urlArr[1])
+	function createFile({ w, h, isGrayscale = false, blur }) {
 		let inputString = `./public/${random}_${w}_${h}.jpg`
 
 		let inStream = fs.createReadStream(imageData[random])
@@ -37,19 +59,21 @@ app.get("*", function (req, res) {
 			console.log("Error")
 		})
 
-		outStream.on("close", function () {
-			console.log("Saved the output file.")
-		})
-
 		let transform = sharp()
 			.resize({ width: w, height: h })
-			// .pipelineColourspace("rgb16")
+			.grayscale(isGrayscale)
+			.blur(blur)
 			.on("info", function () {
 				res.set("Content-Type", "image/jpeg")
 				inStream.pipe(transform).pipe(res)
 			})
 
 		inStream.pipe(transform).pipe(outStream)
+
+		outStream.on("close", function () {
+			fs.unlinkSync(inputString)
+			console.log("Saved the output file.")
+		})
 	}
 })
 
